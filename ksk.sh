f926585f90f52292e848d2b425f5d83c8b89d501
nrosamalave <nrosamalave@gmail.com>
2024-05-29 10:52:11 -0400

    Update ksk.sh

--- ksk.sh
+++ ksk.sh
@@ -1,11 +1,27 @@
-!/bin/bash
-# Get the list of domain names from the user.
+#!/bin/bash
+
+# Prompt the user to choose between nameserver 1 or 2
+echo "Choose the nameserver to use:"
+read -p "Enter 1 or 2: " nameserver_choice
+
+# Set the nameserver based on user choice
+if [ "$nameserver_choice" == "1" ]; then
+  nameserver="ns10.digicertdns.com"
+elif [ "$nameserver_choice" == "2" ]; then
+  nameserver="ns20.digicertdns.com"
+else
+  echo "Invalid choice. Exiting."
+  exit 1
+fi
+
+# Get the list of domain names from the user
 echo "Enter a list of domain names, separated by spaces:"
 read domain_names
-# Set the nameserver to use.
-nameserver="208.94.148.159"
-# Loop through the list of domain names.
+
+# Loop through the list of domain names
 for domain in $domain_names; do
-  # Run a dig against the DNSKEY and grep the KSK for the domain.
-  dig @$nameserver $domain dnskey  +nocomments +noauthority +noadditional | grep "257"
+  echo "Domain: $domain"
+  # Run a dig against the DNSKEY and grep the KSK (key signing key) for the domain
+  dig @$nameserver $domain dnskey +nocomments +noauthority +noadditional | grep "257"
+  echo
 done
